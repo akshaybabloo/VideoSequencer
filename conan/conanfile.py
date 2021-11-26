3524c9fec8dcb26da7e83351f2a8578e8f99906a
Akshay Raj Gollahalli <akshay@gollahalli.com>
2021-11-27 11:40:11 +1300

    conan moved to txt format

--- conan/conanfile.py
+++
@@ -1,9 +0,0 @@
-from conans import ConanFile
-
-
-class VideoSequencerConan(ConanFile):
-    name = "VideoSequencer"
-    version = "1.0.0"
-    settings = "os", "compiler", "build_type", "arch"
-    requires = "opencv/4.5.3"
-    generators = "cmake_find_package", "cmake", "cmake_paths"
