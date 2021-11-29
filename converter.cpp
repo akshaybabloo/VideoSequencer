f8039239441015799e1f9a779588248b9ebe763e
Akshay Raj Gollahalli <akshay@gollahalli.com>
2021-11-29 20:46:31 +1300

    write image init

--- converter.cpp
+++ converter.cpp
@@ -5,6 +5,8 @@
 #include "converter.h"
 #include <QDebug>
 #include <QMessageBox>
+#include <boost/range/irange.hpp>
+#include <boost/range/algorithm_ext/push_back.hpp>
 
 Converter::Converter(QObject *parent, QList<QString> *filePaths) : QObject(parent) {
     _filePaths = filePaths;
@@ -21,6 +23,8 @@ void Converter::convertToFrames() {
 
     for (const QString& filePath: *_filePaths) {
         qDebug() << filePath;
+        std::vector<int> frameIndexes;
+        Mat frame;
 
         VideoCapture capture(filePath.toStdString());
 
@@ -29,15 +33,25 @@ void Converter::convertToFrames() {
             return;
         }
 
-        auto frameCount = capture.get(cv::CAP_PROP_FRAME_COUNT);
+        int frameCount = int(capture.get(cv::CAP_PROP_FRAME_COUNT));
         auto fps = capture.get(cv::CAP_PROP_FPS);
-
         auto duration = frameCount/fps;
+        int nthFrame = int((frameCount-10)/10);
+
+        boost::push_back(frameIndexes, boost::irange(1, frameCount, nthFrame));
 
         qDebug()
                 << "fps = " << fps << "\n"
                 << "number of frames = " << frameCount << "\n"
-                << "duration = " << duration;
+                << "duration = " << duration
+                << "frames = " << frameIndexes;
+
+        for (int index : frameIndexes) {
+            capture.set(cv::CAP_PROP_POS_FRAMES, index);
+            capture >> frame;
+            qDebug() << frame.data;
+            imwrite(QString("test%d").arg(index).toStdString(), frame);
+        }
 
         capture.release();
     }
