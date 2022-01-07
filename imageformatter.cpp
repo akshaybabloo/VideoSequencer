6b57f071d903ae6cce237342ba67675531154fef
Akshay Raj Gollahalli <akshay@gollahalli.com>
2022-01-08 10:31:00 +1300

    QtConcurrent init

--- imageformatter.cpp
+++ imageformatter.cpp
@@ -2,6 +2,10 @@
 // Created by em8273 on 30/11/2021.
 //
 
+#include <QImage>
+#include <QtConcurrent>
+#include <QFuture>
+#include <QDebug>
 #include "imageformatter.h"
 
 ImageFormatter::ImageFormatter(QObject *parent) : QObject(parent) {
@@ -10,4 +14,18 @@ ImageFormatter::ImageFormatter(QObject *parent) : QObject(parent) {
 
 QImage ImageFormatter::createImageCollage(ImageData *imageData) {
 
+    QFuture<QImage> future = QtConcurrent::mapped(imageData->getFrames(), ImageFormatter::scaled);
+    auto item = future.then([](const QImage& reply){
+        qDebug() << reply;
+    });
+
+    return {};
+}
+
+QImage ImageFormatter::scaled(const QImage &image) {
+    if (image.isNull()){
+        qDebug()<<image;
+        return image;
+    }
+    return image.scaled(100,100);
 }
